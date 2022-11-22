1b7885d349a093fbf1494719cb61ba92dc10b7d6
allbs <ad@allbs.pt>
2022-11-22 12:48:44 -0100

    Add project files.

---
+++ ValidarNif/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ValidarNif
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool wait = false;
+            do
+            {
+                Console.Write("Nif: ");
+
+                string nif = Console.ReadLine();
+
+                if (nif == "") { wait = true; }
+
+                Console.WriteLine(PtNifValidation.Nif(nif));
+                
+            } while (!wait);
+
+        }
+    }
+}
---
+++ ValidarNif/PtNifValidation.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ValidarNif
+{
+    public class PtNifValidation
+    {
+        public static bool Nif(string nifNumber)
+        {
+            int maxNumberSize = 9;
+
+            string filteredNumber = Regex.Match(nifNumber, @"[0-9]+").Value;
+
+            if (filteredNumber.Length != maxNumberSize || int.Parse(filteredNumber[0].ToString()) == 0) { return false; }
+
+            int checkSum = 0;
+           
+            for (int i = 0; i < maxNumberSize-1; i++)
+            {
+                checkSum += (int.Parse(filteredNumber[i].ToString()))*(maxNumberSize-i);
+            }
+
+            int checkDigit = 11-(checkSum % 11);
+           
+            if (checkDigit > 9) { checkDigit = 0; }
+
+            return checkDigit == int.Parse(filteredNumber[maxNumberSize - 1].ToString());
+
+        }
+
+    }
+}
